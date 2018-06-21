52d447b0a8a878c324078d9053cdc21ee6c3b741
Sunpetch Jungvisitthon <gapkun.genius@gmail.com>
2018-06-21 18:38:41 +0900

    fix student again

--- src/Frontend/Student.js
+++ src/Frontend/Student.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { width } from 'window-size';
 import PropTypes from 'prop-types';
+import { Success } from './student/Success'
 import { Footer } from './Footer';
 import { ProfileField } from './student/ProfileField';
 import { CourseField } from './student/CourseField';
@@ -14,76 +15,91 @@ import { Button, Container, Row, Col, Card, CardImg, CardBody, CardTitle, CardSu
     Prop src must be qu
 */
 const defaultValue = {
-  UserID: 'EX111',
-  Username: 'Play_Play',
-  FirstName: 'John',
-  LastName: 'Doe',
-  Email: 'John_doe1234@gg.com',
+  UserID: '',
+  Username: '',
+  FirstName: '',
+  LastName: '',
+  Email: '',
   ProfileImg: 'http://www.uv.mx/sin-humo/files/2014/06/Ponentes.png',
-  Birthday: '1550-11-12',
-  Address: '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
-  Gender: 'Male',
+  Birthday: '',
+  Address: '',
+  Gender: '',
+  src: [{CourseID:'',CourseName:'',CourseExpDate:'',CourseLink:''}]
+}
+
+var testData = {
+  UserID: 'Success111',
+  Username: 'Loggin in',
+  FirstName: 'Jonathan',
+  LastName: 'Boooo',
+  Email: 'Jonathan_Boo0000@kk.co.th',
+  ProfileImg: 'http://www.uv.mx/sin-humo/files/2014/06/Ponentes.png',
+  Birthday: '1999-05-22',
+  Address: '123456 abcdef 543210 ijklmnop',
+  Gender: 'Female',
   src: [
     { CourseID: '10001', CourseName: 'Math for Programmer 0', CourseExpDate: '2018-10-11', CourseLink: '' },
     { CourseID: '10002', CourseName: 'Math for Programmer 1', CourseExpDate: '2018-07-11', CourseLink: '' },
     { CourseID: '10003', CourseName: 'Math for Programmer 2', CourseExpDate: '2018-06-10', CourseLink: '' },
     { CourseID: '10004', CourseName: 'Math for Programmer 3', CourseExpDate: '2018-02-07', CourseLink: '' }
   ]
-
 }
+
 export class Student extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { step: 2 }
-    this.toEditPage = this.toEditPage.bind(this);
-    this.toProfilePage = this.toProfilePage.bind(this);
-    this.getDatabaseValue();
+    this.state = { step: 1 }
+    this.toNextStep = this.toNextStep.bind(this);
+    this.toPreviousStep = this.toPreviousStep.bind(this);
+    this.saveValues = this.saveValues.bind(this);
+    this.getDatabaseValue = this.getDatabaseValue.bind(this);
   }
 
   getDatabaseValue() {
     //get Data from database
-    defaultValue.UserID = 'Success111';
-    defaultValue.Username = 'Loggin in';
-    defaultValue.FirstName = 'Jonathan';
-    defaultValue.LastName = 'Boooo';
-    defaultValue.Email= 'Jonathan_Boo0000@kk.co.th',
-    defaultValue.ProfileImg = 'http://www.uv.mx/sin-humo/files/2014/06/Ponentes.png';
-    defaultValue.Birthday = '1999-05-22';
-    defaultValue.Address = '123456 abcdef 543210 ijklmnop';
-    defaultValue.Gender = 'Female';
-    defaultValue.src = [
-      { CourseID: '10001', CourseName: 'Math for Programmer 0', CourseExpDate: '2018-10-11', CourseLink: '' },
-      { CourseID: '10002', CourseName: 'Math for Programmer 1', CourseExpDate: '2018-07-11', CourseLink: '' },
-      { CourseID: '10003', CourseName: 'Math for Programmer 2', CourseExpDate: '2018-06-10', CourseLink: '' },
-      { CourseID: '10004', CourseName: 'Math for Programmer 3', CourseExpDate: '2018-02-07', CourseLink: '' }
-    ]
+    defaultValue.UserID = testData.UserID;
+    defaultValue.Username = testData.Username;
+    defaultValue.FirstName = testData.FirstName;
+    defaultValue.LastName = testData.LastName;
+    defaultValue.Email = testData.Email;
+    defaultValue.ProfileImg = testData.ProfileImg;
+    defaultValue.Birthday = testData.Birthday;
+    defaultValue.Address = testData.Address;
+    defaultValue.Gender = testData.Gender;
+    defaultValue.src = testData.src;
   }
 
-  toEditPage() {
-    var newStep = (this.state.step + 1) > 2 ? 2 : (this.state.step + 1);
+  toNextStep() {
+    var newStep = (this.state.step + 1) > 3 ? 3 : (this.state.step + 1);
 
     this.setState({
       step: newStep
     })
   }
 
-  toProfilePage() {
+  toPreviousStep() {
     var newStep = (this.state.step - 1) < 1 ? 1 : (this.state.step - 1);
-
+    console.log(this.state.step);
     this.setState({
       step: newStep
     })
+  }
+
+  saveValues(field) {
+    return (
+      testData = Object.assign({}, testData, field)
+    )
   }
 
   render() {
-
     switch (this.state.step) {
       case 1:
+        this.getDatabaseValue();
         return (
           <div className='App'>
             <Container fluid style={{ backgroundColor: '#222', bottom: 0, padding: 20 }}>
               <Col sm="12" md={{ size: 8, offset: 2 }}>
-                <ProfileField toEditPage={this.toEditPage} defaultValue={defaultValue} />
+                <ProfileField toNextStep={this.toNextStep} defaultValue={defaultValue} />
                 <CourseField defaultValue={defaultValue} />
               </Col>
             </Container>
@@ -95,10 +111,16 @@ export class Student extends React.Component {
           <div className='App'>
             <Container fluid style={{ backgroundColor: '#222', bottom: 0, padding: 20 }}>
               <Col sm="12" md={{ size: 8, offset: 2 }}>
-                <EditProfileField toProfilePage={this.toProfilePage} defaultValue={defaultValue} />
+                <EditProfileField toPreviousStep={this.toPreviousStep} toNextStep={this.toNextStep} defaultValue={defaultValue} saveValues={this.saveValues} />
               </Col>
             </Container>
             <Footer />
+          </div>
+        );
+      case 3:
+        return (
+          <div className='App'>
+            <Success />
           </div>
         );
       default:

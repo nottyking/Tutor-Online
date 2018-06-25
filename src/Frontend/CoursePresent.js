c248eb9f478b9cc4a1af348198d141030a46a0f4
sippakorn9 <sippakorn9@gmail.com>
2018-06-25 14:40:14 +0700

    edit coursepresent

--- src/Frontend/CoursePresent.js
+++ src/Frontend/CoursePresent.js
@@ -18,11 +18,11 @@ export class CoursePresent extends React.Component {
         const ListItems = src.map(
             (item, i) => <Col>
                 <Card key={i} style={{ width: 332, marginTop: 20, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',border:'none' }}>
-                    <CardImg src={item.Cimage} alt="Card image cap" />
+                    <CardImg src={item.courseImage} alt="Card image cap" />
                     <CardBody>
-                        <CardTitle>{item.Cname}</CardTitle>
-                        <CardText>{item.Cdesc}</CardText>
-                        <Button href={item.CLink}>Register</Button>
+                        <CardTitle>{item.courseName}</CardTitle>
+                        <CardText>{item.courseDesc}</CardText>
+                        <Button href={item.courseLink}>Register</Button>
                     </CardBody>
                 </Card>
             </Col>

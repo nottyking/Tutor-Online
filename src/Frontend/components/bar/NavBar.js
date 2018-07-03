b19c960b744449fc550e086cb53939b17faafa1a
Sunpetch Jungvisitthon <gapkun.genius@gmail.com>
2018-07-03 17:15:18 +0900

    add admin nav

--- src/Frontend/components/bar/NavBar.js
+++ src/Frontend/components/bar/NavBar.js
@@ -5,26 +5,22 @@ import Logout from '../loginPanel/Logout';
 import Login from '../loginPanel/Login';
 
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Label
+    Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Label, Popover, PopoverBody
 } from 'reactstrap';
 
 const pages = ['home', 'about us', 'Register'];
 const userPages = ['home', 'student', 'learning', 'about us'];
 const adminPages = ['home', 'student', 'learning', 'about us', 'admin'];
+const adminFunctionPage = ['manage User', 'manage Course'];
 
 export default class NavBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isOpen: false, };
+        this.state = { isOpen: false, popoverOpen: false };
         this.toggle = this.toggle.bind(this);
         this.createPage = this.createPage.bind(this);
+        this.createAdminPage = this.createAdminPage.bind(this);
+        this.togglePopover = this.togglePopover.bind(this);
     }
 
     toggle() {
@@ -33,46 +29,76 @@ export default class NavBar extends React.Component {
         });
     }
 
+    togglePopover() {
+        this.setState({
+            popoverOpen: !this.state.popoverOpen
+        });
+    }
+
+
+    //TO create navbar page in each user type
     createPage(contents) {
         const listPage = contents.map(page => {
             var link;
-            switch (page) {
-                case 'home':
-                    link = '';
-                    break;
-                case 'about us':
-                    link = "about_us";
-                    break;
-                default:
-                    link = page;
-                    break;
+            if (page != 'admin') {
+                switch (page) {
+                    case 'home':
+                        link = '';
+                        break;
+                    default:
+                        link = page.replace(" ", "_");
+                        break;
+                }
+                return (
+                    <NavItem>
+                        <NavLink tag={Link} to={'/' + link} exact>{page.toUpperCase()}</NavLink>
+                    </NavItem>
+                );
+            } else {
+                return this.createAdminPage(adminFunctionPage);
             }
-            return (
-                <NavItem>
-                    <NavLink tag={Link} to={'/' + link} exact>{page.toUpperCase()}</NavLink>
-                </NavItem>
-            );
-        }
-        );
+        });
         return listPage;
+    }
+
+    //TO create dropdown navbar admin
+    createAdminPage(content) {
+        const adminListPage = content.map(page => {
+            var link = page.replace(" ", "_");
+            return (
+                <NavLink tag={Link} to={'/admin_' + link} exact>{page.toUpperCase()}</NavLink>
+            );
+        });
+        return (
+            <div>
+                <NavLink navbar id="adminPopover" onClick={this.togglePopover}>
+                    ADMIN
+                </NavLink>
+                <Popover placement="bottom-start"
+                    isOpen={this.state.popoverOpen}
+                    target="adminPopover"
+                    toggle={this.togglePopover}>
+                    <PopoverBody>{adminListPage}</PopoverBody>
+                </Popover>
+            </div>
+        );
     }
 
     render() {
         var actionButton;
         var pageList;
-        var userType;
         const user = localStorage.getItem('user')
 
         if (!user) {
-            userType = "Guest"
+            //userType = "Guest"
             pageList = this.createPage(pages);
             actionButton = <Login loginStatus={this.props.loginStatus} />
-        }else if (JSON.parse(user).role == 0) {
-            userType = "user"
+        } else if (JSON.parse(user).role === 0) {
+            //userType = "user"
             pageList = this.createPage(userPages);
             actionButton = <Logout loginStatus={this.props.loginStatus} />
-        } else if (JSON.parse(user).role == 1) {
-            userType = "admin"
+        } else if (JSON.parse(user).role === 1) {
+            //userType = "admin"
             pageList = this.createPage(adminPages);
             actionButton = <Logout loginStatus={this.props.loginStatus} />
         }
--- src/Frontend/components/router/BrowserRouterManager.js
+++ src/Frontend/components/router/BrowserRouterManager.js
@@ -20,26 +20,28 @@ class BrowserRouterManager extends React.Component {
         const user = localStorage.getItem('user');
         if (!user) {
             userType = "guest";
-        } else if (JSON.parse(user).role === "0") {
+        } else if (JSON.parse(user).role === 0) {
             userType = "user";
-        } else if (JSON.parse(user).role === "1") {
+        } else if (JSON.parse(user).role === 1) {
             userType = "admin";
         } else {
             userType = "unauthorized"
         }
-
         return (
             <div>
                 <Switch>
                     <Route exact path="/" component={Content} />
                     <Route exact path="/about_us" component={AboutUs} />
-                    <Route exact path="/admin" component={(userType === "admin") ? Admin : () => { return (<Redirect to={'/'} />) }} />
                     <Route exact path="/course/:courseID" component={CourseA} />
+                    
                     <Route exact path="/student" component={(userType === "user" || userType === "admin") ? Student : () => { return (<Redirect to={'/loginPage'} />) }} />
                     <Route exact path="/learning" component={(userType === "user" || userType === "admin") ? Learning : () => { return (<Redirect to={'/loginPage'} />) }} />
                     <Route exact path="/learning/:courseID/:subcourseID" component={(userType === "user" || userType === "admin") ? Learning : () => { return (<Redirect to={'/loginPage'} />) }} />
                     <Route exact path="/register" component={!(userType === "user" || userType === "admin") ? RegisterPage : () => { return (<Redirect to={'/'} />) }} />
                     <Route exact path='/loginPage' component={!(userType === "user" || userType === "admin") ? LoginPage : () => { return (<Redirect to={'/'} />) }} />
+                    
+                    <Route exact path="/admin" component={(userType === "admin") ? Admin : () => { return (<Redirect to={'/'} />) }} />
+                    <Route exact path="/admin_manage_Course" component={(userType === "admin") ? Admin : () => { return (<Redirect to={'/'} />) }} />
                     <Route component={Content} />
                 </Switch>
             </div >

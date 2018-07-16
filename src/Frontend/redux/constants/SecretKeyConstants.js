e000fb96bdcfb69c841f5192f84822918ba5f7f0
Grazyper SJ <gapkun.genius@gmail.com>
2018-07-16 10:51:26 +0700

    move table inside boundary

--- src/Frontend/redux/constants/SecretKeyConstants.js
+++ src/Frontend/redux/constants/SecretKeyConstants.js
@@ -1,5 +1,5 @@
 export const secretKeyConstant = {
     USER_SECRETKEY: 'SJag3SRNY:LWNg24er47s6hgGRyMLJnoaisbesbltwai3458r6toiETiaTo6532eihTtoewathO363e26ltWLTlwTiohTE72A4/LYeryHPEyHWETY45',
-    TOKEN_SECRETKEY: 'Not used yet',
-    DEFAULT_SECRETKEY: 'Not used yet'
+    TOKEN_SECRETKEY: 'asmgaeTSahmfa;mh$$RhTOKENdsahrhklnEnRSY:ORNYRNTMerhlnldnthape"Nrphg238y1i3rfLSglrOgbRKGBKRBGkburbgtou24oRgareh4ae4g',
+    DEFAULT_SECRETKEY: 'Not_used_yet'
 }

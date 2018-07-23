94c513cb3016c68526d515d4dc3a991bf200ea23
nottyking <nottyking153@gmail.com>
2018-07-23 14:57:45 +0700

    add enrolled info of package page

--- src/Backend/Controller/Package/PackageMainController.js
+++ src/Backend/Controller/Package/PackageMainController.js
@@ -7,6 +7,8 @@ async function queryInformation(req, res){
   var courseInformation = (await getFunc.getFunction('courseid,coursename','course WHERE courseid in (\
                             SELECT courseid FROM packagecourse WHERE packageid = ' + packageid + ')',[],[])).result;
   courseInformation = await addCourseLink(courseInformation);
+  courseInformation = await addCourseEnrolled(courseInformation,req);
+
   return {
     package : packageInformation ,
     course : courseInformation
@@ -16,6 +18,23 @@ async function queryInformation(req, res){
 async function addCourseLink(courseInfomation){
   for(var i = 0 ; i < courseInfomation.length ; i++){
     courseInfomation[i].courselink =  "/course/" + courseInfomation[i].courseid;
+  }
+  return courseInfomation;
+}
+
+async function addCourseEnrolled(courseInfomation,req){
+  var userid = -1
+  if(req.session.userid)
+    userid = req.session.userid
+  var enrolledCourse = (await getFunc.getFunction('courseid,expireddate','enrolledcourse',['userid'],[userid])).result;
+  for(var i = 0 ; i < courseInfomation.length ; i++){
+    courseInfomation[i].expireddate =  null
+    for(var j = 0 ; j < enrolledCourse.length ; j++){
+      if(enrolledCourse[j].courseid == courseInfomation[i].courseid){
+        courseInfomation[i].expireddate = enrolledCourse[j].expireddate;
+        break;
+      }
+    }
   }
   return courseInfomation;
 }

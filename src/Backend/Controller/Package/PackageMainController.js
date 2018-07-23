32c238e64926699b1952e462801a25ccf49e708a
nottyking <nottyking153@gmail.com>
2018-07-23 15:13:44 +0700

    fix query package

--- src/Backend/Controller/Package/PackageMainController.js
+++ src/Backend/Controller/Package/PackageMainController.js
@@ -3,7 +3,7 @@ const getFunc = require('../utilityfunction/GetDataSpecial')
 async function queryInformation(req, res){
   console.log("Enter queryInformation in Package<aincontroller");
   const packageid = req.body.packageid;
-  const packageInformation = (await getFunc.getFunction('*','package',['packageid'],[packageid])).result;
+  const packageInformation = (await getFunc.getFunction('*','package',['packageid'],[packageid])).result[0];
   var courseInformation = (await getFunc.getFunction('courseid,coursename','course WHERE courseid in (\
                             SELECT courseid FROM packagecourse WHERE packageid = ' + packageid + ')',[],[])).result;
   courseInformation = await addCourseLink(courseInformation);

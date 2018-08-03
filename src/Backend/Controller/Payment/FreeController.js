29b4db62015d9d0c4cf2519b7eecdb5ed1aad20f
nottyking <nottyking153@gmail.com>
2018-08-03 10:38:45 +0700

    fix

--- src/Backend/Controller/Payment/FreeController.js
+++ src/Backend/Controller/Payment/FreeController.js
@@ -2,10 +2,12 @@ const omise = require('../../Config/Omise');
 const ipList = require('../../../Config/ipConfig');
 const insertFunc = require('../utilityfunction/InsertData')
 
-async function enrollFree(userid, courseid){
+async function enrollFree(req, res){
   // console.log();
   // console.log("req.session:",req.session);
   var d = new Date();
+  var courseid = req.body.courseid;
+  var userid = req.session.userid;
   var expireddate = new Date(d.getFullYear() + 1 , d.getMonth() , d.getDate())
   console.log("userid:",userid);
   console.log("courseid:",courseid);
